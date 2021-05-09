5d207457920f66e7c577f0c86bff8cb4d67de31d
bbenedict <bbenedict@gmail.com>
2021-05-09 16:12:08 -0700

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,43 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env) => {
+  return {
+    mode: process.env.MODE || 'development',
+    performance: {
+      hints: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        }, {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader",
+          },
+        }, {
+          test: /\.(jpg|png|svg)$/,
+          use: {
+            loader: "url-loader",
+          },
         },
-      }, {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader",
-        },
-      }, {
-        test: /\.(jpg|png|svg)$/,
-        use: {
-          loader: "url-loader",
-        },
-      },
+      ],
+    },
+    output: {
+      publicPath: "/",
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+      }),
     ],
-  },
-  output: {
-    publicPath: "/",
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    }),
-  ],
+  };
 };
